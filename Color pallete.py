226727a25e7f315753699b47eb5013b4854696d4
alpha <ayoub.ouaddi.yt@gmail.com>
2026-07-14 17:22:03 +0100

    Add files via upload

---
+++ Color pallete.py
@@ -0,0 +1,18 @@
+from colorama import Style , Fore , init , Back
+print(f"{Back.BLACK}      {Style.RESET_ALL}"
+      f"{Back.RED}      {Style.RESET_ALL}"
+      f"{Back.GREEN}      {Style.RESET_ALL}"
+      f"{Back.YELLOW}      {Style.RESET_ALL}")
+print(f"{Back.BLUE}      {Style.RESET_ALL}"
+      f"{Back.MAGENTA}      {Style.RESET_ALL}"
+      f"{Back.WHITE}      {Style.RESET_ALL}"
+      f"{Back.LIGHTBLACK_EX}      {Style.RESET_ALL}")
+print(f"{Back.LIGHTCYAN_EX}      {Style.RESET_ALL}"
+      f"{Back.LIGHTMAGENTA_EX}      {Style.RESET_ALL}"
+      f"{Back.LIGHTBLUE_EX}      {Style.RESET_ALL}"
+      f"{Back.LIGHTGREEN_EX}      {Style.RESET_ALL}")
+print(f"{Back.LIGHTRED_EX}      {Style.RESET_ALL}"
+      f"{Back.LIGHTWHITE_EX}      {Style.RESET_ALL}"
+      f"{Back.LIGHTYELLOW_EX}      {Style.RESET_ALL}")
+
+       
